1d9640227c5221fb5feb4fc434e4e8c4a787ecbd
Matthewpc12 <aldabamateo81@gmail.com>
2026-02-16 22:12:53 -0600

    refactor: Remove deprecated App.tsx
    This commit removes the unused and deprecated `App.tsx` file. The main application component is now located at `src/App.tsx`.

--- App.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-// This files is deprecated and should not be used. The main App component is located in src/App.tsx
-export default function App() {
-  return null;
-}
